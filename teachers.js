eabed12c8bdcc9bc819518677e61c808979ba7bb
Pedro Felipe Baltar <pfbaltariead@gmail.com>
2020-08-31 16:14:51 -0300

    desafio 4-5 concluido

--- teachers.js
+++ teachers.js
@@ -92,3 +92,55 @@ exports.edit = (req, res) => {
 
   return res.render('teachers/edit', { teacher })
 }
+
+exports.update = (req, res) => {
+  const { id } = req.body
+  let index = 0
+
+  const foundTeacher = data.teachers.find((teacher, foundIndex) => {
+    if(id == teacher.id) {
+      index = foundIndex
+
+      return true
+    }
+  })
+
+  if(!foundTeacher) {
+    return res.send('Teacher not found!')
+  }
+
+  const teacher = {
+    ...foundTeacher,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+    id: Number.parseInt(req.body.id)
+  }
+
+  data.teachers[index] = teacher
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+    if(err) {
+      return res.send('Write file error!')
+    }
+
+    return res.redirect(`/teachers/${id}`)
+  })
+}
+
+exports.delete = (req, res) => {
+  const { id } = req.body
+
+  const filteredTeachers = data.teachers.filter(teacher => {
+    return teacher.id != id
+  })
+
+  data.teachers = filteredTeachers
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+    if(err) {
+      return res.render('Write file error!')
+    }
+
+    return res.redirect('/teachers')
+  })
+}
