e0d526f1b8caaa4f4a306830b061569c51305adf
Pedro Felipe Baltar <pfbaltariead@gmail.com>
2020-09-25 19:10:34 -0300

    app/controllers

--- src/app/controllers/students.js
+++ src/app/controllers/students.js
@@ -1,140 +1,41 @@
-const fs = require('fs')
-const data = require('../data.json')
-const { age, date, grade } = require('../until')
+const { age, date, grade } = require('../../lib/utils')
 
-exports.index = (req, res) => {
-  const students = data.students
+module.exports = {
+  index(req, res) {
+    return res.render('students/index')
+  },
+  create(req, res) {
+    return res.render('students/create')
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body)
 
-  for(let student of students) {
-    student.grade = grade(student.education_level)
-  }
-
-  return res.render('students/index', { students: data.students })
-}
-
-exports.create = (req, res) => {
-  return res.render('students/create')
-}
-
-exports.post = (req, res) => {
-  const keys = Object.keys(req.body)
-
-  // Validação
-  for (key of keys) {
-    if(req.body[key] == "") {
-      return res.send('Por favor, preencha todos os campos!')
-    }
-  }
-
-  birth = Date.parse(req.body.birth)
-
-  let id = 1
-  const lastStudent = data.students[data.students.length - 1]
-
-  if(lastStudent) {
-    id = lastStudent.id + 1
-  }
-
-  data.students.push({
-    id,
-    ...req.body,
-    birth
-  })
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-    if(err) {
-      return res.send('Ocorreu um erro, tente novamente!')
+    for (key of keys) {
+      if(req.body[key] == "") {
+        return res.send('Por favor, preencha todos os campos!')
+      }
     }
 
-    return res.redirect('/students')
-  })
-}
+    return
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  update(req, res) {
+    const keys = Object.keys(req.body)
 
-exports.show = (req, res) => {
-  const { id } = req.params
-  const foundStudent = data.students.find((student) => {
-    return student.id == id
-  })
-
-  if(!foundStudent) {
-    return ('Student not found!')
-  }
-
-  const student = {
-    ...foundStudent,
-    grade: grade(foundStudent.education_level),
-    age: age(foundStudent.birth),  
-    birthDay: date(foundStudent.birth).birthDay
-  }
-
-  return res.render('students/show', { student })
-}
-
-exports.edit = (req, res) => {
-  const { id } = req.params
-  const foundStudent = data.students.find((student) => {
-    return student.id == id
-  })
-
-  if(!foundStudent) {
-    return ('Student not found!')
-  }
-
-  const student = {
-    ...foundStudent,
-    birth: date(foundStudent.birth).iso   
-  }
-
-  return res.render('students/edit', { student })
-}
-
-exports.update = (req, res) => {
-  const { id } = req.body
-  let index = 0
-
-  const foundStudent = data.students.find((student, foundIndex) => {
-    if(id == student.id) {
-      index = foundIndex
-
-      return true
-    }
-  })
-
-  if(!foundStudent) {
-    return res.send('Student not found!')
-  }
-
-  const student = {
-    ...foundStudent,
-    ...req.body,
-    birth: Date.parse(req.body.birth),
-    id: Number.parseInt(req.body.id)
-  }
-
-  data.students[index] = student
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if(err) {
-      return res.send('Write file error!')
+    for (key of keys) {
+      if(req.body[key] == "") {
+        return res.send('Por favor, preencha todos os campos!')
+      }
     }
 
-    return res.redirect(`/students/${id}`)
-  })
-}
-
-exports.delete = (req, res) => {
-  const { id } = req.body
-  const filteredStudents = data.students.filter(student => {
-    return student.id != id
-  })
-
-  data.students = filteredStudents
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if(err) {
-      return res.render('Write file error!')
-    }
-
-    return res.redirect('/students')
-  })
+    return
+  },
+  delete(req, res) {
+    return
+  }
 }
--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,145 +1,41 @@
-const fs = require('fs')
-const data = require('../data.json')
-const { age, date, graduation, classType } = require('../until')
+const { age, date, grade } = require('../../lib/utils')
 
-exports.index = (req, res) => {
-  return res.render('teachers/index', { teachers: data.teachers })
-}
+module.exports = {
+  index(req, res) {
+    return res.render('teachers/index')
+  },
+  create(req, res) {
+    return res.render('teachers/create')
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body)
 
-exports.create = (req, res) => {
-  return res.render('teachers/create')
-}
-
-exports.post = (req, res) => {
-  const keys = Object.keys(req.body)
-
-  // Validação
-  for (key of keys) {
-    if(req.body[key] == "") {
-      return res.send('Por favor, preencha todos os campos!')
-    }
-  }
-  
-  let {
-    avatar_url,
-    name,
-    education_level,
-    classes,
-    services,
-    birth
-  } = req.body
-
-  birth = Date.parse(birth)
-  created_at = Date.now()
-  id = Number(data.teachers.length + 1)
-
-  data.teachers.push({
-    id,
-    created_at,
-    avatar_url,
-    name,
-    education_level,
-    classes,
-    services,
-    birth
-  })
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-    if(err) {
-      return res.send('Ocorreu um erro, tente novamente!')
+    for (key of keys) {
+      if(req.body[key] == "") {
+        return res.send('Por favor, preencha todos os campos!')
+      }
     }
 
-    return res.redirect('/teachers')
-  })
-}
+    return
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  update(req, res) {
+    const keys = Object.keys(req.body)
 
-exports.show = (req, res) => {
-  const { id } = req.params
-  const foundTeacher = data.teachers.find((teacher) => {
-    return teacher.id == id
-  })
-
-  if(!foundTeacher) {
-    return ('Teacher not found!')
-  }
-
-  const teacher = {
-    ...foundTeacher,
-    services: foundTeacher.services.split(','),
-    age: age(foundTeacher.birth),
-    classType: classType(foundTeacher.classes),
-    graduation: graduation(foundTeacher.education_level),    
-    created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at)
-  }
-
-  return res.render('teachers/show', { teacher })
-}
-
-exports.edit = (req, res) => {
-  const { id } = req.params
-  const foundTeacher = data.teachers.find((teacher) => {
-    return teacher.id == id
-  })
-
-  if(!foundTeacher) {
-    return ('Teacher not found!')
-  }
-
-  const teacher = {
-    ...foundTeacher,
-    birth: date(foundTeacher.birth).iso    
-  }
-
-  return res.render('teachers/edit', { teacher })
-}
-
-exports.update = (req, res) => {
-  const { id } = req.body
-  let index = 0
-
-  const foundTeacher = data.teachers.find((teacher, foundIndex) => {
-    if(id == teacher.id) {
-      index = foundIndex
-
-      return true
-    }
-  })
-
-  if(!foundTeacher) {
-    return res.send('Teacher not found!')
-  }
-
-  const teacher = {
-    ...foundTeacher,
-    ...req.body,
-    birth: Date.parse(req.body.birth),
-    id: Number.parseInt(req.body.id)
-  }
-
-  data.teachers[index] = teacher
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if(err) {
-      return res.send('Write file error!')
+    for (key of keys) {
+      if(req.body[key] == "") {
+        return res.send('Por favor, preencha todos os campos!')
+      }
     }
 
-    return res.redirect(`/teachers/${id}`)
-  })
-}
-
-exports.delete = (req, res) => {
-  const { id } = req.body
-  const filteredTeachers = data.teachers.filter(teacher => {
-    return teacher.id != id
-  })
-
-  data.teachers = filteredTeachers
-
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if(err) {
-      return res.render('Write file error!')
-    }
-
-    return res.redirect('/teachers')
-  })
+    return
+  },
+  delete(req, res) {
+    return
+  }
 }
