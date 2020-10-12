77571e845c974e9b181d8249afef00723f0f2a8f
Pedro Felipe Baltar <pfbaltariead@gmail.com>
2020-10-12 20:44:19 -0300

    Teachers done

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -3,17 +3,42 @@ const Teacher = require('../models/Teacher')
 
 module.exports = {
   index(req, res) {
-    const { filter } = req.query
+    let {
+      filter,
+      page,
+      limit
+    } = req.query
 
-    if(filter) {
-      Teacher.findBy(filter, teachers => {
-        return res.render('teachers/index', { teachers, filter })
-      })
-    } else {
-      Teacher.all(teachers => {
-        return res.render('teachers/index', { teachers })
-      })
+    page = page || 1
+    limit = limit || 2
+    let offset = limit * (page - 1)
+
+    const params = {
+      filter,
+      page,
+      limit,
+      offset,
+      callback(teachers) {
+        const pagination = {
+          total: Math.ceil(teachers[0].total / limit),
+          page
+        }
+
+        return res.render('teachers/index', { teachers, pagination, filter })
+      }
     }
+
+    Teacher.paginate(params)
+
+    // if(filter) {
+    //   Teacher.findBy(filter, teachers => {
+    //     return res.render('teachers/index', { teachers, filter })
+    //   })
+    // } else {
+    //   Teacher.all(teachers => {
+    //     return res.render('teachers/index', { teachers })
+    //   })
+    // }
   },
   create(req, res) {
     return res.render('teachers/create')
